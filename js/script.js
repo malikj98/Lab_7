af8b6ad792572e1420a643be8df62d9ecc16acae
malikj98 <70449964+malikj98@users.noreply.github.com>
2020-11-27 16:10:14 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@ function init(){
  function js() {
    var button = document.getElementById("entrybutton");
    var input = document.getElementById("entryinput").value;
+   var name = document.getElementsByTagName("h2").item(0).innerHTML;
 	alert("Malik Roberson: " + input);
 	document.getElementById("textoutput").innerHTML = input
 }
+button.addEventListener("click", js);
 window.addEventListener('load', init);
 }
