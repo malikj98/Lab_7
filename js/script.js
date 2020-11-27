0f516d3c77417ba0e6b40ac737e526490616b309
malikj98 <70449964+malikj98@users.noreply.github.com>
2020-11-27 16:24:48 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
 //add your javascript between these two lines of code
- document.getElementById("entrybutton").addEventListener("click", js);
+ var button = document.getElementById("entrybutton")
  function js() {
    var input = document.getElementById("entryinput").value;
    var name = document.getElementsByTagName("h2").item(0).innerHTML;
-	alert(name + ": " + input);
-	document.getElementById("textoutput").innerHTML = input
+   alert(name + ": " + input);
+   document.getElementById("textoutput").innerHTML = input
+}
+button.addEventListener("click", one);
 }
 window.addEventListener('load', init);
-}
