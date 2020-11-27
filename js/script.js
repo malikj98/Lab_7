18041a231167b7c304ad758912a11771bd0bffe5
malikj98 <70449964+malikj98@users.noreply.github.com>
2020-11-27 16:42:45 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init() {
-//add your javascrip between these two lines of code
-  document.getElementById("entrybutton").addEventListener("click", lab);
-  function lab() {
-    var button = document.getElementById("entrybutton");
-    var input = document.getElementById("entryinput").value;
-      alert(" Malik Roberson: " + input);
-      document.getElementById("textoutput").innerHTML = input;
-  }
+//add your javascript between these two lines of code
+    var enbutton = document.getElementById("entrybutton");
+    function one() {
+        var eninput = document.getElementById("entryinput").value;
+        var name = document.getElementsByTagName("h2").item(0).innerHTML;
+        alert(name + ": " + eninput);
+        document.getElementById("textoutput").innerHTML = eninput;
+    }
+    enbutton.addEventListener("click", one);
+}
 window.addEventListener('load', init);
-} 
